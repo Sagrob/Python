8d74cc6008b3da8a4f2259b720035ed632a23365
Sagrob <136131254+Sagrob@users.noreply.github.com>
2024-02-26 08:35:14 -0300

    Update

---
+++ desconto.py
@@ -0,0 +1,5 @@
+venda = float(input("Qual o valor da venda?"))
+desconto = float(input("Qual a porcentagem do desconto?"))
+
+resultado = (desconto / venda)*100
+print("O valor com desconto será: $resultado")
