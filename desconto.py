9a99abe36f932a8689ef214c77b2b4451521eb8c
Sagrob <136131254+Sagrob@users.noreply.github.com>
2024-02-26 08:37:08 -0300

    Update desconto.py

--- desconto.py
+++ desconto.py
@@ -1,5 +1,7 @@
+while True:
+    try:
 venda = float(input("Qual o valor da venda?"))
 desconto = float(input("Qual a porcentagem do desconto?"))
 
 resultado = (desconto / venda)*100
-print("O valor com desconto será: $resultado")
+print("O valor com desconto será: ", resultado)
