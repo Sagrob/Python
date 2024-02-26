d2fee2eae7e5d13ce64cb97e6ff71ca23654af5b
Sagrob <136131254+Sagrob@users.noreply.github.com>
2024-02-26 08:37:53 -0300

    Update desconto.py

--- desconto.py
+++ desconto.py
@@ -1,7 +1,10 @@
 while True:
     try:
-venda = float(input("Qual o valor da venda?"))
-desconto = float(input("Qual a porcentagem do desconto?"))
-
+        venda = float(input("Qual o valor da venda?"))
+        desconto = float(input("Qual a porcentagem do desconto?"))
+    except:
+        print("Por favor, digite um número")
+        continue
 resultado = (desconto / venda)*100
 print("O valor com desconto será: ", resultado)
+break
